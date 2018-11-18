bf19b47c097080dccbc1f20069fb09a7b3a5e09d
John Haugeland <stonecypher@gmail.com>
2018-11-17 23:09:47 -0800

    markdown based site generator

---
+++ src/build_js/markdownit.js
@@ -0,0 +1,41 @@
+
+let curFM = {};
+
+const fs    = require('fs'),
+      glob  = require('glob'),
+
+      head  = fs.readFileSync('./src/html/head.frag.html'),
+      mid   = fs.readFileSync('./src/html/mid.frag.html'),
+      foot  = fs.readFileSync('./src/html/foot.frag.html'),
+
+      mIt   = require('markdown-it'),
+      mFA   = require('markdown-it-fontawesome'),
+      mIn   = require('markdown-it-include'),
+      mU    = require('markdown-it-underline'),
+      mFM   = require('markdown-it-front-matter'),
+
+      md    = mIt({ html: true /*, highlight: (code, lang) => code */ })
+               .use(mIn, './src/md/gamma/')
+               .use(mU)
+               .use(mFM, fm => {
+                  curFM = {};
+                  fm.trim().split('\n').map(line => {
+                    const [l,r]     = line.split(':', 2);
+                    curFM[l.trim()] = r.trim();
+                  })
+               })
+//             .use(mFM, fm => curFM = fm)
+               .use(mFA);
+
+// const files = glob.sync('./src/md/**/*.md');
+// console.log(JSON.stringify(files));
+
+glob.sync('./src/md/**/*.md').map(f => {
+
+  const uHtml  = md.render( `${fs.readFileSync(f)}` ),
+        uTitle = curFM.title,
+        uSlug  = curFM.slug;
+
+  fs.writeFileSync(`./docs/${uSlug}`, `${head}${uTitle}${mid}${uHtml}${foot}`, 'utf-8');
+
+});
---
+++ src/md/index.md
@@ -0,0 +1,5 @@
+---
+title: FSL.tools - your first stop for Finite State Language
+slug: index.html
+---
+# Welcome to FSL Tools
